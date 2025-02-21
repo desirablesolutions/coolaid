7cb162158419b1782c6b326960492a9f0e30e847
Jeffrey Desir <8616589+desirtechnology@users.noreply.github.com>
2025-02-21 20:55:15 +0000

    [t]

--- app/next.config.ts
+++ app/next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
